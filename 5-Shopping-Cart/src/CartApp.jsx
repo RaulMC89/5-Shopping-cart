059d24f1325e50df5e05332cf009f302ac762b87
Raul <91200250+RaulMC89@users.noreply.github.com>
2024-06-15 14:08:48 +0200

    Credo objeto producto, services, un map para cada columna componente producto

--- 5-Shopping-Cart/src/CartApp.jsx
+++ 5-Shopping-Cart/src/CartApp.jsx
@@ -1,68 +1,30 @@
+import { useEffect, useState } from "react";
+import { getProducts } from "./services/productServices"
+
 export const CartApp = () => {
+    const [products, setProducts] = useState([]);
+
+    useEffect(() => {
+        setProducts(getProducts());
+
+    }, []);
+
     return (<>
         <div className="container">
             <h3>Cart App</h3>
             <div className="row">
-                <div className="col-4 my-2">
-                    <div className="card">
-                        <div className="card-body">
-                            <h5 className="card-title">Teclado Mecanico</h5>
-                            <p className="card-text">Teclado mecanico con luces RGB switches cherry red</p>
-                            <p className="card-text">25 €</p>
-                            <button className="btn btn-primary">Agregar</button>
+                {products.map(product => (
+                    <div className="col-4 my-2" key={product.id}>
+                        <div className="card">
+                            <div className="card-body">
+                                <h5 className="card-title">{product.name}</h5>
+                                <p className="card-text">{product.description}</p>
+                                <p className="card-text">{product.price} €</p>
+                                <button className="btn btn-primary">Agregar</button>
+                            </div>
                         </div>
                     </div>
-                </div>
-                <div className="col-4 my-2">
-                    <div className="card">
-                        <div className="card-body">
-                            <h5 className="card-title">Pantalla 17 pulgadas</h5>
-                            <p className="card-text">Pantalla 1080P con refresco instataneo</p>
-                            <p className="card-text">265 €</p>
-                            <button className="btn btn-primary">Agregar</button>
-                        </div>
-                    </div>
-                </div>
-                <div className="col-4 my-2">
-                    <div className="card">
-                        <div className="card-body">
-                            <h5 className="card-title">Memoria Corseir 1GB</h5>
-                            <p className="card-text">Memoria Ram con velocidad de escritura 4900MB y de lectura 3200Mb</p>
-                            <p className="card-text">115 €</p>
-                            <button className="btn btn-primary">Agregar</button>
-                        </div>
-                    </div>
-                </div>
-                <div className="col-4 my-2">
-                    <div className="card">
-                        <div className="card-body">
-                            <h5 className="card-title">Raton RGB</h5>
-                            <p className="card-text">Raton con luces RGB switches cherry red</p>
-                            <p className="card-text">33 €</p>
-                            <button className="btn btn-primary">Agregar</button>
-                        </div>
-                    </div>
-                </div>
-                <div className="col-4 my-2">
-                    <div className="card">
-                        <div className="card-body">
-                            <h5 className="card-title">Silla gamer</h5>
-                            <p className="card-text">Con reposabrazos adaptables y reposa cabezas ajustable</p>
-                            <p className="card-text">367 €</p>
-                            <button className="btn btn-primary">Agregar</button>
-                        </div>
-                    </div>
-                </div>
-                <div className="col-4 my-2">
-                    <div className="card">
-                        <div className="card-body">
-                            <h5 className="card-title">Mesa apatable 2x1,5</h5>
-                            <p className="card-text">Mesa adaptable en altura para mejorar la circulacion</p>
-                            <p className="card-text">279 €</p>
-                            <button className="btn btn-primary">Agregar</button>
-                        </div>
-                    </div>
-                </div>
+                ))}
             </div>
             <div className="my-4 w-50">
                 <h3>Carro de compras</h3>
---
+++ 5-Shopping-Cart/src/data/products.js
@@ -0,0 +1,38 @@
+export const products = [ 
+    {
+        id: 1,
+        name: 'Pantalla 17 pulgadas',
+        description: 'Pantalla 1080P con refresco instataneo',
+        price: 265,
+    },
+    {
+        id: 2,
+        name: 'Memoria Corser 1GB',
+        description: 'Memoria Ram con velocidad de escritura 4900MB y de lectura 3200Mb',
+        price: 115,
+    },
+    {
+        id: 3,
+        name: 'Raton RGB',
+        description: 'Raton con luces RGB switches cherry red',
+        price: 33,
+    },
+    {
+        id: 4,
+        name: 'Teclado Mecanico',
+        description: 'Teclado mecanico con luces RGB switches cherry red',
+        price: 25,
+    },
+    {
+        id: 5,
+        name: 'Silla gamer',
+        description: 'Con reposabrazos adaptables y reposa cabezas ajustable',
+        price: 279,
+    },
+    {
+        id: 6,
+        name: 'Mesa apatable 2x1,5',
+        description: 'Mesa adaptable en altura para mejorar la circulacion',
+        price: 367,
+    }
+]
