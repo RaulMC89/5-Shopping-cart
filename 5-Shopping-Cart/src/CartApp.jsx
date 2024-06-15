afb9b702ddb52edfd23e2056bb4478b7a729d781
Raul <91200250+RaulMC89@users.noreply.github.com>
2024-06-15 09:55:37 +0200

    Componente principal CartAPP

---
+++ 5-Shopping-Cart/src/CartApp.jsx
@@ -0,0 +1,69 @@
+export const CartApp = () => {
+    return (<>
+        <div className="container">
+            <h3>Cart App</h3>
+            <div className="row">
+                <div className="col-4 my-2">
+                    <div className="card">
+                        <div className="card-body">
+                            <h5 className="card-title">Teclado Mecanico</h5>
+                            <p className="card-text">Teclado mecanico con luces RGB switches cherry red</p>
+                            <p className="card-text"></p>
+                            <button className="btn btn-primary">Agregar</button>
+                        </div>
+                    </div>
+                </div>
+                <div className="col-4 my-2">
+                    <div className="card">
+                        <div className="card-body">
+                            <h5 className="card-title">Teclado Mecanico</h5>
+                            <p className="card-text">Teclado mecanico con luces RGB switches cherry red</p>
+                            <p className="card-text"></p>
+                            <button className="btn btn-primary">Agregar</button>
+                        </div>
+                    </div>
+                </div>
+                <div className="col-4 my-2">
+                    <div className="card">
+                        <div className="card-body">
+                            <h5 className="card-title">Teclado Mecanico</h5>
+                            <p className="card-text">Teclado mecanico con luces RGB switches cherry red</p>
+                            <p className="card-text"></p>
+                            <button className="btn btn-primary">Agregar</button>
+                        </div>
+                    </div>
+                </div>
+                <div className="col-4 my-2">
+                    <div className="card">
+                        <div className="card-body">
+                            <h5 className="card-title">Teclado Mecanico</h5>
+                            <p className="card-text">Teclado mecanico con luces RGB switches cherry red</p>
+                            <p className="card-text"></p>
+                            <button className="btn btn-primary">Agregar</button>
+                        </div>
+                    </div>
+                </div>
+                <div className="col-4 my-2">
+                    <div className="card">
+                        <div className="card-body">
+                            <h5 className="card-title">Teclado Mecanico</h5>
+                            <p className="card-text">Teclado mecanico con luces RGB switches cherry red</p>
+                            <p className="card-text"></p>
+                            <button className="btn btn-primary">Agregar</button>
+                        </div>
+                    </div>
+                </div>
+                <div className="col-4 my-2">
+                    <div className="card">
+                        <div className="card-body">
+                            <h5 className="card-title">Teclado Mecanico</h5>
+                            <p className="card-text">Teclado mecanico con luces RGB switches cherry red</p>
+                            <p className="card-text"></p>
+                            <button className="btn btn-primary">Agregar</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </>)
+}
