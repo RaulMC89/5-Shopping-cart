13cc68cd358e5a67f283cbc25a74f5b3f7e8d504
Raul <91200250+RaulMC89@users.noreply.github.com>
2024-06-15 10:26:17 +0200

    Cambio de parametro en las Cartas

--- 5-Shopping-Cart/src/CartApp.jsx
+++ 5-Shopping-Cart/src/CartApp.jsx
@@ -8,7 +8,7 @@ export const CartApp = () => {
                         <div className="card-body">
                             <h5 className="card-title">Teclado Mecanico</h5>
                             <p className="card-text">Teclado mecanico con luces RGB switches cherry red</p>
-                            <p className="card-text"></p>
+                            <p className="card-text">$ 25</p>
                             <button className="btn btn-primary">Agregar</button>
                         </div>
                     </div>
@@ -16,9 +16,9 @@ export const CartApp = () => {
                 <div className="col-4 my-2">
                     <div className="card">
                         <div className="card-body">
-                            <h5 className="card-title">Teclado Mecanico</h5>
-                            <p className="card-text">Teclado mecanico con luces RGB switches cherry red</p>
-                            <p className="card-text"></p>
+                            <h5 className="card-title">Pantalla 17 pulgadas</h5>
+                            <p className="card-text">Pantalla 1080P con refresco instataneo</p>
+                            <p className="card-text">$ 265</p>
                             <button className="btn btn-primary">Agregar</button>
                         </div>
                     </div>
@@ -26,9 +26,9 @@ export const CartApp = () => {
                 <div className="col-4 my-2">
                     <div className="card">
                         <div className="card-body">
-                            <h5 className="card-title">Teclado Mecanico</h5>
-                            <p className="card-text">Teclado mecanico con luces RGB switches cherry red</p>
-                            <p className="card-text"></p>
+                            <h5 className="card-title">Memoria Corseir 1GB</h5>
+                            <p className="card-text">Memoria Ram con velocidad de escritura 4900MB y de lectura 3200Mb</p>
+                            <p className="card-text">$ 115</p>
                             <button className="btn btn-primary">Agregar</button>
                         </div>
                     </div>
@@ -36,9 +36,9 @@ export const CartApp = () => {
                 <div className="col-4 my-2">
                     <div className="card">
                         <div className="card-body">
-                            <h5 className="card-title">Teclado Mecanico</h5>
-                            <p className="card-text">Teclado mecanico con luces RGB switches cherry red</p>
-                            <p className="card-text"></p>
+                            <h5 className="card-title">Raton RGB</h5>
+                            <p className="card-text">Raton con luces RGB switches cherry red</p>
+                            <p className="card-text">$ 33</p>
                             <button className="btn btn-primary">Agregar</button>
                         </div>
                     </div>
@@ -46,9 +46,9 @@ export const CartApp = () => {
                 <div className="col-4 my-2">
                     <div className="card">
                         <div className="card-body">
-                            <h5 className="card-title">Teclado Mecanico</h5>
-                            <p className="card-text">Teclado mecanico con luces RGB switches cherry red</p>
-                            <p className="card-text"></p>
+                            <h5 className="card-title">Silla gamer</h5>
+                            <p className="card-text">Con reposabrazos adaptables y reposa cabezas ajustable</p>
+                            <p className="card-text">$ 367</p>
                             <button className="btn btn-primary">Agregar</button>
                         </div>
                     </div>
@@ -56,9 +56,9 @@ export const CartApp = () => {
                 <div className="col-4 my-2">
                     <div className="card">
                         <div className="card-body">
-                            <h5 className="card-title">Teclado Mecanico</h5>
-                            <p className="card-text">Teclado mecanico con luces RGB switches cherry red</p>
-                            <p className="card-text"></p>
+                            <h5 className="card-title">Mesa apatable 2x1,5</h5>
+                            <p className="card-text">Mesa adaptable en altura para mejorar la circulacion</p>
+                            <p className="card-text">$ 279</p>
                             <button className="btn btn-primary">Agregar</button>
                         </div>
                     </div>
